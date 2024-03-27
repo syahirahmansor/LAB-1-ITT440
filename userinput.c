6be0b661f67ebf3d0502a069b52af79d913409a4
syahirah1 <syahirah1@syahirah1.myguest.virtualbox.org>
2024-03-28 00:04:27 +0800

    My second program in C

---
+++ userinput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+/* This is my seconf program in C */
+int age;
+printf("Hi, how old are you?>");
+scanf("%d", &age);
+printf("You are %d years old", age);
+return (0);
+}
+
